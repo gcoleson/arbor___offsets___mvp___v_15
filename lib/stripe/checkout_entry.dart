20014cbaca8c000f01fa0691f921b04e9ed8ffb7
agyscon <agsyconner67@gmail.com>
2020-11-03 12:37:05 -0500

    Tests with stripe checkout

--- lib/stripe/checkout_entry.dart
+++ lib/stripe/checkout_entry.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'one_time_checkout.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class checkout_entry extends StatelessWidget {
   @override
@@ -17,7 +18,11 @@ class checkout_entry extends StatelessWidget {
 
               // First Ping Firebase for session ID for stripe checkout
               final http.Response response = await http.post(
-                  'https://us-central1-financeapp-2c7b8.cloudfunctions.net/payment/');
+                'https://us-central1-financeapp-2c7b8.cloudfunctions.net/payment/',
+                body: json.encode(
+                  {'price': 1337, 'product': 'A flight'},
+                ),
+              );
 
               //then decode the json returned
               if (response.body != null && response.body != 'error') {
