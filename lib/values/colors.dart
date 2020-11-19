b31d8b48408dca6099de4ff78b3812a11d90ffd8
agyscon <agsyconner67@gmail.com>
2020-11-19 15:21:47 -0500

    added color and font files

--- lib/values/colors.dart
+++ lib/values/colors.dart
@@ -8,7 +8,6 @@
 
 import 'dart:ui';
 
-
 class AppColors {
   static const Color primaryBackground = Color.fromARGB(255, 216, 216, 216);
   static const Color secondaryBackground = Color.fromARGB(255, 65, 127, 69);
@@ -19,4 +18,35 @@ class AppColors {
   static const Color primaryText = Color.fromARGB(255, 2, 2, 2);
   static const Color secondaryText = Color.fromARGB(255, 65, 127, 69);
   static const Color accentText = Color.fromARGB(255, 250, 195, 21);
+
+  static const Color primaryDarkGreen = Color.fromARGB(255, 65, 127, 69);
+  static const Color secondaryLightGreen = Color.fromARGB(255, 93, 187, 71);
+  static const Color highlightYellow = Color.fromARGB(255, 250, 195, 21);
+  static const Color backgroundBlue = Color.fromARGB(255, 28, 151, 211);
+  static const Color backgroundGrey = Color.fromARGB(255, 237, 236, 228);
+  static const Color borderGrey = Color.fromARGB(255, 151, 151, 151);
+  static const Color highlightBlue = Color.fromARGB(255, 41, 190, 226);
+  static const Color darkGrey = Color.fromARGB(255, 67, 82, 88);
+  static const Color white = Color.fromARGB(255, 255, 255, 255);
+  static const Color Black = Color.fromARGB(255, 0, 0, 0);
+  static const Color transparentScreen = Color.fromARGB(127, 216, 216, 216);
+  static const Color brownGrey = Color.fromARGB(255, 142, 137, 107);
+  static const Color mediumGreyScreen = Color.fromARGB(92, 60, 60, 67);
+  static const Color lightGreenScreen = Color.fromARGB(50, 120, 120, 128);
+
+//these are the original Hex values (to double-check in Zeplin or Sketch:
+  //primaryDarkGreen = #417F45
+  //secondaryLightGreen = #5DBB47
+  // highlightYellow = #FAC315
+  // backgroundBlue = #1C97D3
+  // backgroundGrey = #EDECE4
+  // borderGrey = #979797
+  // highlightBlue = #29BEE2
+  // darkGrey = #435258
+  // white = #FFFFFF
+  // black = #000000
+  // purchaseTileGrey = #D8D8D8 (alpha 100)
+  // brownGrey = #8e896b
+  // mediumGreyScreen = #3c3c43 (alpha 36)
+  // lightGreenScreen = #787880 (alpha 20)s
 }
