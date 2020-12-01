3140818d7416f1b13ba08684561d8e0ff769861e
agyscon <agsyconner67@gmail.com>
2020-12-01 13:09:05 -0500

    enabled notifications for the app

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@
 import 'package:arbor___offsets___mvp___v_15/onboarding_screens/onboard_main_screen.dart';
 import 'package:arbor___offsets___mvp___v_15/projects_widget/projects_widget.dart';
 import 'package:arbor___offsets___mvp___v_15/services/database.dart';
+import 'package:arbor___offsets___mvp___v_15/services/push_notifications.dart';
 import 'package:arbor___offsets___mvp___v_15/tab_group_one_tab_bar_widget/tab_group_one_tab_bar_widget.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 FirebaseAnalytics analytics;
 
@@ -52,6 +54,10 @@ void main() async {
 
   // Enabling analytics for Firebase
   analytics = FirebaseAnalytics();
+
+  PushNotificaitonsManager pushNotifs = new PushNotificaitonsManager();
+
+  pushNotifs.init();
 
   print('App Version:' + appVersion + ' Date:' + appDate);
 
